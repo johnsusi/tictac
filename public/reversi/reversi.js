a1bafc52c0e2c10ed935ad02bc78b164bed9f832
John Susi <john@susi.se>
2013-12-03 21:32:22 +0100

    Updated readme with new project name.

--- public/reversi/reversi.js
+++ public/reversi/reversi.js
@@ -2,47 +2,70 @@ var dx = 512 >> 3;
 var dy = dx;
 var width = dx * 8;
 var height = dy * 8;
-var game = {
-    id: 0,
-    me: {},
-    state: {
-        nextState: 0,
-        board: []
-    }
-};
+//var game;
+var me, you, lastMoveWasPass = false;
 
 
-var dim = Math.min($(window).width(), $(window).height());
+var POLL_INTERVALL = 40000;
 
 //d3.select('body').attr('width', dim).attr('height', dim);
+console.clear();
 
-var svg = d3.select('body').append('svg:svg')
-    .attr('width', dim - 10)
-    .attr('height', dim - 10)
-    .attr('viewBox', '0,0,512,512')
-;
 
-$(window).resize(function () {
-    console.log("resize: " + $(window).width() + ", " + $(window).height());
-    var dim = Math.min($(window).width(), $(window).height());
-//    dx = dim >> 3;
-//    dy = dx;
-//    width = dx * 8;
-//    height = dy * 8;
-    svg.attr('width', dim-10).attr('height', dim-10);
-    update();
-});
-var update = function () {
-    console.log("dx = " + dx + ", dy = " + dy);
+function model(o, properties) {
+
+	var observers = [];
+	var proxy = Object.create(o);
+	proxy.addObserver = function (observer) { 
+		observers.push(observer); 
+		return this;
+	};
+	proxy.removeObserver = function (observer) {
+		observers = observers.filter(function (el) {
+			el !== observer; 
+		});
+		return this;
+	};
+	
+	function firePropertyChanged(value) {
+		observers.forEach(function (observer) { observer(value); });
+	}	
+	
+	var values = {};
+
+	properties.forEach(function (property) {
+		values[property] = o[property];
+		Object.defineProperty(proxy, property, {
+			get: function() { return values[property]; },
+			set: function (value) {
+				firePropertyChanged(values[property] = value);
+			}
+		});
+	});
+
+	return proxy;
+}
+
+$(function () {
+
+	console.log("start");
+	var game;
+	var dim = Math.min($(window).width(), $(window).height());
+	var svg = d3.select('body').append('svg:svg')
+    		.attr('width', dim - 10)
+		    .attr('height', dim - 10)
+		    .attr('viewBox', '-24,-24,560,560')
+	;
+
     svg.selectAll('rect')
-        .data(game.state.board)
+        .data(new Array(64))
         .enter().append('rect')
                 .attr('x', function (d, i) { return (i %  8) * dx; })
                 .attr('y', function (d, i) { return (i >> 3) * dy; })
                 .attr('width',  dx)
                 .attr('height', dy)
-                .attr('rx', 5)
-                .attr('ry', 5)
+                .attr('rx', 5.5)
+                .attr('ry', 5.5)
                 .style('fill', function (d, i) {
                     var x = i % 8;
                     var y = i >> 3;
@@ -51,92 +74,319 @@ var update = function () {
     ;
 
 
-    var circles = svg.selectAll('circle').data(game.states.board);
-	var validMoves = findValidMoves(game.states.
 
-    circles.enter().append('circle')
-            .attr('cx', function (d, i) { return dx / 2 + (i %  8) * dx; })
-            .attr('cy', function (d, i) { return dy / 2 + (i >> 3) * dy; })
-            .attr('r', dx / 2.5)
-            .attr('stroke', "black")
+var dataset = "ABCDEFGH".split("");
 
-    circles .classed('black', function (d) { return d == '*'; })
-            .classed('white', function (d) { return d == 'O'; })
-            .classed('empty', function (d) { return d == '.'; })
-            .classed('valid', function (d, i) { return d == '.' && game.states.validMoves.indexOf(i) != -1; })
-            .on('click', function (d, i) {
-                if (game.states.validMoves.indexOf(i) != -1) {
-                    $.ajax({
-                        type: 'POST',
-                        data: { move: i, me: game.state.player == 0 ? game.me.id : game.you.id},
-                        dataType: 'json',
-                        url: 'games/' + game.id + '/queue/',
-                        success: function (data) {
-                        }
-                    });
-                }
-            })
-    ;
-    
-    circles.exit().remove();
-    
-};
+var xScale = d3.scale.ordinal()
+    .domain("ABCDEFGH".split(""))
+    .rangeRoundBands([0, 512], 0.05);
 
-$.ajax({
-        type: 'POST',
-        data: { name: "I am white" },
-        dataType: 'json',
-        url: 'games/',
-        success: function (data) {
-            game.id = data.id;
-            game.me = data.you;
-       
-            $.ajax({
-                    type: 'POST',
-                    data: { 
-							name: "I am black",
-				            state: {
-									board: [
-          		'.', '.', '.', '.', '.', '.', '.', '.',
-                '.', '.', '.', '.', '.', '.', '.', '.',
-                '.', '.', '.', '.', '.', '.', '.', '.',
-                '.', '.', '.', '*', 'O', '.', '.', '.',
-                '.', '.', '.', 'O', '*', '.', '.', '.',
-                '.', '.', '.', '.', '.', '.', '.', '.',
-                '.', '.', '.', '.', '.', '.', '.', '.',
-                '.', '.', '.', '.', '.', '.', '.', '.',
-						            ],
-						            player: 0,
-						            nextState: 1
-							}
-					},
-                    dataType: 'json',
-                    url: 'games/' + game.id + '/players/',
-                    success: function (data) {
-                        game.you = data;
-                        (function poll() {
-                            $.ajax({
-                                    type: 'GET',
-                                    url: 'games/' + game.id + '/states/' + game.state.nextState,
-                                    success: function (data) {
-                                        if (typeof data === "object") {                                
-                                            game.state = data;
-                                            update();
-                                        }
-                                    },
-                                    complete: function (jqXHR, status) {
-                                        if (status == "success" || status == "timeout") poll();
-                                        else setTimeout(poll, 15000);
-                                    },
-                                    timeout: 30000
-                            });
-                        })();
-                    }
-            });
-        }
+var yScale = d3.scale.ordinal()
+	.domain("12345678".split(""))
+	.rangeRoundBands([0, 512], 0.05);
+
+var xAxis = d3.svg.axis().scale(xScale).orient("top");
+var yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "x bottom axis")
+	.attr("transform", "translate(0, 512)")
+    .call(xAxis);
+
+
+svg.append("g")
+	.attr("class", "y axis")
+	.call(yAxis);
+
+svg.append("g")
+	.attr("class", "y right axis")
+	.attr("transform", "translate(536, 0)")
+ 	.call(yAxis);
+
+
+	$(window).resize(function () {
+	    var dim = Math.min($(window).width(), $(window).height());
+		svg.attr('width', dim-10).attr('height', dim-10);
+	});
+
+
+	create(function (data) {	
+
+		game = model(data, ['state']).addObserver(function () {
+			update(svg, game); 
+			if (game.state.player != me.index) {
+				poll(svg, game);
+			}
+		});	
+
+		console.log(game.states);
+
+if (0) {
+		game.addObserver(function () {
+			if (('next' in game.state) && game.state.player == 0) {
+				console.log("doing white move");
+				doRandomMove(game);
+			}
+		});
+		game.addObserver(function () {
+			if (('next' in game.state) && game.state.player == 1) {
+				console.log("doing black move");
+				doRandomMove(game);
+			}
+		});
+}
+
+
+if (0) {
+			findPlayers(game, function (data) {
+				me = { index: -1 };
+				game.state = game.states.last();
+
+			});
+}
+
+		join(game, 'I am White', function (data) {
+			me = data;
+
+			findPlayers(game, function (data) {
+				game.state = game.states.last();
+				poll(svg, game);
+			});
+
+
+		});
+	});
 });
 
+// Library
 
+function findPlayers(game, callback) {
+	put('/lobby/' + game.id, { 'type': 'reversi/1.0', 'players': 2 })
+			.done(function(data, status) {
+		switch (status) {
+			case 'nocontent': return findPlayers(game, callback);
+			default: return callback(data);
+		}
+		
+	});
+	
+}
+
+
+function update(svg, game) {
+	
+	var state = game.state;
+	var p = state.player == 0 ? 'O' : '*';
+	var o = p == 'O' ? '*' : 'O';
+    var circles = svg.selectAll('use');
+			
+	var myTurn = me.index == state.player;
+
+	var validMoves = findValidMoves(state.board,p,o,'.');
+	dumpMoves(validMoves);
+	var b = state.board.split("").map(function (el, i) {
+	var res = {
+			el: (!myTurn || validMoves.indexOf(i) == -1) ? el : '?',
+			x: i % 8, 
+			y: i >> 3
+		};
+		console.log(i + " -> " + JSON.stringify(res));
+		return res;
+		}).filter(function (el) {
+		return el.el == 'O' || el.el == '*' || el.el == '?';
+	});
+	
+
+	circles = circles.data(b, function (d) {
+		var key = d.x + d.y * 8;		
+	 	switch (d.el) {
+			case 'O': return key;
+			case '*': return 64+key;
+			default: return 128 + key;
+		}
+	});
+
+	circles.enter()
+			.append('use').attr('xlink:href', 
+				function (d, i) {
+					switch (d.el) {
+						case '*': return '#blackchip';
+						case 'O': return '#whitechip';
+						case '?': return p == '*' ? '#blackchip' : '#whitechip';
+					}
+				})
+			.attr("transform", "translate(-100, -100)")
+			.transition().duration(1000)
+			.attr("transform", function (d) {
+				return "translate(" + (d.x * dx) + ", " + 
+					(d.y * dy) + ")";
+			})
+	;
+
+    circles .classed('black', function (d) { return d.el == '*'; })
+            .classed('white', function (d) { return d.el == 'O'; })
+			.classed('valid', function (d) { return d.el == '?'; })
+            .on('click', function (d, i) {
+				if (d.el == '?') {
+					return move(game, d.x + d.y * 8);
+				}
+			});
+    
+    circles.exit()
+			.transition().duration(1000)
+			.attr('transform', 'translate(-100, -100)')
+			.remove();
+   
+}
+
+function post(url, data) {
+	return send('POST', url, data);
+}
+
+function put(url, data) {
+	return send('PUT', url, data);
+}
+
+function send(method, url, data) {
+	if (typeof data !== 'string') data = JSON.stringify(data);
+	var keyid = 'hello';
+	var secret = 'world';
+	var signature = CryptoJS.HmacSHA1(data,secret)
+			.toString(CryptoJS.enc.Base64);
+	return $.ajax({
+			type: method,
+			url: url, 
+	        contentType: 'application/json; charset=utf-8',
+			data: data,
+			dataType: 'json',
+			headers: { 'Authorization': 'TicToc ' + keyid + ':' + signature }
+	});
+
+}
+
+function create(success) {
+
+	post('/games/', { states: [ {
+			board: [
+    	    		'.', '.', '.', '.', '.', '.', '.', '.',
+					'.', '.', '.', '.', '.', '.', '.', '.',
+       	    	    '.', '.', '.', '.', '.', '.', '.', '.',
+          	    	'.', '.', '.', '*', 'O', '.', '.', '.',
+	               	'.', '.', '.', 'O', '*', '.', '.', '.',
+		            '.', '.', '.', '.', '.', '.', '.', '.',
+	                '.', '.', '.', '.', '.', '.', '.', '.',
+	       	        '.', '.', '.', '.', '.', '.', '.', '.',
+			].join(""),
+			player: 0,
+			lastMove: undefined,
+			next: 1 
+	}]}).done(success);
+
+}
+
+function join(game, name, success) {
+	post('/games/' + game.id + '/players/', { 'name': name }).done(success);
+}
+
+function poll(svg, game) {
+	$.ajax({
+    	type: 'GET',
+		url: '/games/' + game.id + '/states/' + game.state.next,
+		statusCode: {
+			204: function () { poll(svg, game); }
+		},
+        timeout: POLL_INTERVALL			
+	}).done(function (data, status) {
+		if (typeof data === "object") {    
+			console.log('setting state from poll');	
+			game.state = data;
+		}
+	}).fail(function (jqXHR, status, errorThrown) {
+		switch (status) {
+			case 'timeout': return poll(svg, game);
+			default: return console.log(status + ' : ' + errorThrown);
+		}				
+	});
+}
+
+function move(game, pos) {
+	var state = Object.clone(game.state, true);
+	var p = state.player == 0 ? 'O' : '*';
+	var o = p == 'O' ? '*' : 'O';
+	var m = 'ABCDEFGH'[pos%8] + '12345678'[pos >>> 3];
+
+	state.board = doMove(state.board, m, p, o, '.'); 
+	state.player = (++state.player) % 2;
+	state.lastMove = m;
+	++state.next;
+	console.log(p + ' : ' + state.lastMove);
+
+	post('/games/' + game.id + '/states/', state).done(function(data) { 
+		game.state = data;
+	});
+}
+
+function pass(game) {
+	var state = Object.clone(game.state, true);
+	var p = state.player == 0 ? 'O' : '*';
+	if (state.lastMove == '--') {
+		$.ajax({
+			type: 'PUT',
+			url: '/games/' + game.id + '/result', 
+	        contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify({'hello': 'world'}), 
+			dataType: 'json'
+		});
+		delete state.player;
+		delete state.next;
+	}
+	else {
+		state.player = (++state.player) % 2;
+		state.lastMove = '--';
+		++state.next;
+	}
+	console.log(p + ' : ' + state.lastMove);
+
+	post('/games/' + game.id + '/states/', state).done(function(data) { 
+		game.state = data;
+	});
+}
+
+function doRandomMove(game) {
+
+	var p = game.state.player == 0 ? 'O' : '*';
+	var o = p == 'O' ? '*' : 'O';
+	var validMoves = findValidMoves(game.state.board,p,o,'.');
+
+	if (validMoves.length > 0) {
+		var m = Math.random() * validMoves.length;
+		m = Math.floor(m);
+		move(game, validMoves[m]);
+	}
+	else pass(game);
+}
+
+// Reversi lib
+
+var moves = [
+	'A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
+	'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
+	'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3',
+	'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4',
+	'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5',
+	'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6',
+	'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7',
+	'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8',
+];
+
+function dumpMoves(m) {
+	console.log(m.map(function (el) { 
+			return moves[el]; }));
+}
 
 function findValidMoves(board, player, opponent, empty) {
 
@@ -175,8 +425,12 @@ function findValidMoves(board, player, opponent, empty) {
 
 }
 
-function doMove(board, x, y, player, opponent, empty) {
+function doMove(board, move, player, opponent, empty) {
     
+	board = board.split("");
+	var x = 'ABCDEFGH'.indexOf(move[0]);
+	var y = '12345678'.indexOf(move[1]);
+
     var flip = function (x, y, dx, dy) {
         for (var xx = x + dx, yy = y + dy, flip = 0;
                 xx >= 0 && xx <  8 &&
@@ -205,6 +459,8 @@ function doMove(board, x, y, player, opponent, empty) {
     flip(x, y, -1, +1);
     flip(x, y, -1,  0);
     flip(x, y, +1,  0);
+
+	return board.join("");
     
 }
 
--- web.js
+++ web.js
@@ -12,31 +12,158 @@
 
 var fs      = require('fs');
 var uuid    = require('node-uuid');
+
+var metrics = require('librato-metrics').createClient({
+	email: 'john@susi.se',
+	token: 'd21cf722053c202911f3c8f354928989ffdfb5eef834f6c19fcdfc25a206800f'
+});
+
 var express = require('express');
 var app     = express();
 
+var crypto = require('crypto');
+var nacl_factory = require("js-nacl");
+var nacl = nacl_factory.instantiate();
+
+require('sugar');
+
 var games = {};
 var pollers = [];
+var lobby = {};
 
 var oneDayInSeconds = 60 * 60 * 24;
 var oneYearInSeconds = oneDayInSeconds * 365;
 
 app.use(express.logger());
-app.use(express.json());
+app.use(express.json({
+	verify: function (req, res, buf) {
+		if ('rawBody' in req) req.rawBody += buf;
+		else req.rawBody = buf;
+		console.log('verify: ' + buf);
+		return true;
+	}		
+}));
 app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
+
+var secrets = {
+
+	'hello': 'world'
+};
+
+function authenticate(req, res, next) {
+
+	console.log(authenticate);
+
+	var authorization = req.get('Authorization');
+
+	if (!authorization) return res.set('WWW-Authenticate', 'TicToc').send(401);
+
+	console.log(authorization);
+	var l = authorization.match(/TicToc ([^:]*):(.*)/);
+	console.log(l);
+	if (!l || l.length != 3) return res.set('WWW-Authenticate', 'TicToc').send(401);
+
+	var keyid     = l[1];
+	var signature = l[2];
+
+	console.log(keyid + ' => ' + signature);
+
+	if (!(keyid in secrets)) return res.set('WWW-Authenticate', 'TicToc')
+			.send(401, 'Key not registered');
+
+	if (!('rawBody' in req)) return res.send(400, 'Missing body');
+	 
+	var hmacsha1 = crypto.createHmac('sha1', secrets[keyid]);
+	hmacsha1.update(req.rawBody);
+	
+	if (signature !== hmacsha1.digest('base64')) return res.set('WWW-Authenticate', 'TicToc')
+			.send(401, 'Signature does not match');
+
+
+	return next();
+
+}
+
+app.post('*', authenticate);
+app.put('*', authenticate);
+app.delete('*', authenticate);
+
+app.post('/verify-auth', function (req, res) {
+
+
+	console.log(req.get('Authorization'));
+	console.log(req.get('Content-Length'));
+
+
+});
+
 
 app.get('/', function (req, res) {
 	return res.send("Hello World!");
 });
+
+app.get('/lobby', function (req, res) {
+	return res.json(lobby);
+});
+
+function expired204(res, timeout) {
+	var timestamp = new Date().getTime();
+	return function() {
+		if (new Date().getTime() >= timestamp + timeout) {
+			res.send(204);
+			return true;
+		}
+		else return false;
+	}
+}
+
+app.put('/lobby/:id', function (req, res) {
+	
+	var game = games[req.params.id];
+	lobby[game.id] = req.body;
+	console.log(lobby);
+	return pollers.push({
+		completed: function() { 
+			if (game.players.length >= req.body.players) {
+				res.json(200, {});
+				return true;
+			}
+			else return false;
+		},
+		expired: expired204(res, 30000),
+		cleanup: function () { delete lobby[game.id]; }
+	});
+});
+
+app.get('/lobby/find', function (req, res) {
+
+	for (var id in lobby) {
+		var entry = lobby[id];
+		var match = true;
+		for (var p in req.query) {
+			if (!entry.hasOwnProperty(p) || 
+					entry[p] != req.query[p]) match = false;
+		}
+		
+		if (match) return res.redirect(302, '/games/' + id);
+
+	}	
+
+	return res.send(204);
+});
+
 
 app.get('/games/', function (req, res) {
     return res.json(games);
 });
 
 app.all('/games/:id*', function (req, res, next) {
-	if (!(req.params.id in games)) return res.send(404, 'Game not found');
-	return next();
+	return (req.params.id in games) ? next() : res.send(404, 'Game not found');
+});
+
+app.post('/games/:id*', function (req, res, next) {
+	return ('result' in games) ? res.send(403, 'Game is over') : next();
 });
 
 app.get('/games/:id', function (req, res) {
@@ -45,24 +172,27 @@ app.get('/games/:id', function (req, res) {
 
 app.post('/games/', function (req, res) {
 
-    var user = {
-        id: uuid.v4()
-	};
+	var game = req.body;
+	
+	if (!('states' in game) || game.states.length == 0) return res.send(400);
 
-    var game = {
-        id: uuid.v4(),
-        players: [
-            { name: req.body.name, user: user }
-        ],
-        states: [ req.body.state ]            
-	};
-    
-    games[game.id] = game;
-    
-    return res.json({
-        id: game.id,
-        you: user.id
-    });
+	game.id = uuid.v4(); 
+	if (!('players' in game)) game.players = [];
+	if (game.players.length > 0) {
+		game.players[0].user = { id: uuid.v4() };
+	}
+
+   	games[game.id] = game;
+ 	   
+    return res.redirect(303, './' + game.id);
+});
+
+app.get('/games/:id/players/:playerid', function (req, res) {
+
+	var game = games[req.params.id];
+
+	return res.json(game.players[req.params.playerid]);
+	
 });
 
 app.post('/games/:id/players/', function (req, res) {
@@ -74,41 +204,46 @@ app.post('/games/:id/players/', function (req, res) {
     var user = {
 		id: uuid.v4(),
 	};
-        
-    game.players.push({
+   
+	var player = {
+		index: game.players.length,
         name: req.body.name,
         user: user
-    });
+    };
+
+
+    game.players.push(player);
                 
-    return res.json({
-		id: game.id, 
-		you: user.id
-	});
+    return res.redirect(303, './' + player.index);
 
 });
 
 app.get('/games/:id/states/latest', function (req, res) { 
   	var game = games[req.params.id];
-	return res.redirect(302, '' + (game.history.length-1));
+	return res.redirect(302, './' + (game.history.length-1));
 });
 
 app.get('/games/:id/states/:state', function (req, res) {
  
     var game = games[req.params.id];
-	
+	var timestamp = new Date().getTime();
+
 	// Comment out to disable long-polling
 	if (req.params.state >= game.states.length ) {
+		if ('result' in game) return res.send(404, 'Game is over');
         return pollers.push({
-            res: res,
-            game: game,
-            state: req.params.state,
-            timestamp: new Date().getTime()
-        });
+			completed: function() { 
+				if (req.params.state < game.states.length) {
+			   		res.json(game.states[req.params.state]); 
+					return true;
+				}
+				else return false;
+			},
+			expired: expired204(res, 10000)
+		});
     }
 
-	return res
-			.header('Cache-Control', 'public, max-age=3600')
-			.json(game.states[req.params.state]);
+	return res.json(game.states[req.params.state]);
 
 });
 
@@ -116,13 +251,34 @@ app.post('/games/:id/states/', function (req, res) {
 	
 	var game = games[req.params.id];
 	game.states.push(req.body);	
-	return res.json(game.states.length);
+	return res.redirect(303, 
+			'./' + (game.states.length - 1));
+});
+
+app.get('/games/:id/result', function (req, res) {
+
+	var game = games[req.params.id];
+	if ('result' in game) res.json(game.result); 
+	else res.send(404, 'Game is not over');
+
+});
+
+app.put('/games/:id/result', function (req, res) {
+
+	var game = games[req.params.id];
+
+	if ('result' in game) return res.header('Allow', 'GET')
+
+	game.result = req.body;
+
+	return res.send(201);
+
 });
 
 app.delete('/games/:id', function (req, res) {
 
 	delete games[req.params.id];
-  	return res.json(true);
+  	return res.send(204);
 });
 
 var port = process.env.PORT || 5000;
@@ -136,20 +292,26 @@ app.listen(port, function() {
 
 	var expiration = new Date().getTime() - 30000; // 30s
    
-    pollers = pollers.filter(function (poller) {
-    
-        if (poller.state < poller.game.states.length) {
-            poller.res.json(poller.game.states[poller.state]);
-            return false;
-        }
-        else if (poller.timestamp < expiration) {
-			poller.res.send(200, 'OK');
-            return false;
-        }
-        else return true;
-    
-    });
-
-    setTimeout(processQueues, 10);
+    pollers = pollers.filter(function (poller) {  
+		if (poller.completed() || poller.expired()) {
+			if ('cleanup' in poller) poller.cleanup();
+			return false;
+		}
+		else return true;
+	});
+    setTimeout(processQueues, 1000);
 })();
 
+(function sendMetrics() {
+
+	metrics.post('/metrics', {
+
+		gauges: [
+			{ name: 'tictoc.pollers', value: pollers.length }
+		]
+		
+	}, function() {});
+
+	setTimeout(sendMetrics, 60000);
+
+})();
